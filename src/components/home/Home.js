c9162545de62c296fb19a4134e76cd24200a3183
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-01-02 03:34:08 -0600

    little styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import booksLink from './booksLink.png';
 import bookshelfstack from './bookshelfstack.png';
 import diverseFriends from './diverseFriends.png';
+import "bootstrap/dist/css/bootstrap.min.css" 
 import "./Home.css"
 
 class Home extends Component {
@@ -10,14 +11,12 @@ class Home extends Component {
         return (
             <React.Fragment>
             <section><h1>Welcome To Bookish So Far!</h1></section>
-            <div className="image-container">
-                <div className="row justify-content-center">
-            <Link to="/books"><img className="imageLinks col-5" src={booksLink}></img></Link>
-            <Link to="/bookshelves"><img className="imageLinks col-5" src={bookshelfstack}></img></Link>
+            <div className="image-container d-flex flex-row align-items-center flex-grow-1">
+                <div className="d-flex min-vw-25 min-vh-25 flex-row justify-content-around">
+            <Link to="/books"><img className="book-icon imageLinks col-5" src={booksLink}></img></Link>
+            <Link to="/bookshelves"><img className="bookshelf-icon imageLinks col-5" src={bookshelfstack}></img></Link>
+            <Link to="/friends"><img className="friends-icon imageLinks" src={diverseFriends}></img></Link>
                 </div>
-                <div className="row justify-content-center">
-            <Link><img className="imageLinks" src={diverseFriends}></img></Link>
-            </div>
             </div>
             
 
--- src/components/shelves/ShelfCard.js
+++ src/components/shelves/ShelfCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Card } from 'react-bootstrap';
+// Making an attempt at styling this thing
 
 class ShelfCard extends Component {
     render () {
@@ -10,12 +11,12 @@ class ShelfCard extends Component {
             <Card>
                 <div className="card">
                     <div className="card-content">
-                    <h1>Shelf Name: {this.props.shelf.shelfName}</h1>
-                    <h1>Shelf Genre: {this.props.shelf.genre}</h1>
+                    <h3>Shelf Name: {this.props.shelf.shelfName}</h3>
+                    <h4>Shelf Genre: {this.props.shelf.genre}</h4>
                     </div>
                     <button type="button" className="btn btn-primary btn-lg" onClick={() => this.props.history.push(`/bookshelves/${this.props.shelf.id}/edit`)}>Edit</button>
                     <br></br>
-                    <button type="button" className="btn btn-secondary btn-lg" onClick={() => this.props.deleteShelf(this.props.shelf.id)}>Delete Book</button>
+                    <button type="button" className="btn btn-secondary btn-lg" onClick={() => this.props.deleteShelf(this.props.shelf.id)}>Delete Shelf</button>
                 </div>
             </Card>
             </>
